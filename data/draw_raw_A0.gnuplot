ca0dba8fd51d75da8d4f7990f56477cfcf68c211
Bernd Brägelmann <bernd@braegelmann.de>
2011-11-16 21:08:01 +0100

    dunkelheit appeared

--- data/draw_raw_A0.gnuplot
+++
@@ -1,4 +0,0 @@
-set datafile separator "|"
-set terminal png
-set output 'A0.png'
-plot "A0.txt"
---
+++ raw_temp2munin.rb
@@ -0,0 +1,11 @@
+#!/usr/local/rvm/bin/ruby
+
+f=File.open("/home/x42/serialreader.git/current/A0",'r') # temperatur sensor (NTC) is at Arduinos analog pin A0
+raw=f.readline
+#calibration                                                                                                                                                           
+# 530 => 23                                                                                                                                                            
+# 616 => 15                                                                                                                                                            
+# 408 => 34                                                                                                                                                            
+# => m =-0.0913                                                                                                                                                        
+# => b =71.28                                                                                                                                                          
+puts "Temp.value #{raw.to_i*(-0.0913)+71.28}\n"
